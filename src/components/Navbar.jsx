f838296f292c3627273771e8d5e3f02e5ba5c3cf
Antoni Ambroż <polandhagacz@gmail.com>
2025-03-09 22:24:28 +0100

    Dodanie hamb menu dla telefonow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaUser, FaEnvelope } from 'react-icons/fa';
+import { FaHome, FaUser, FaEnvelope, FaBars, FaTimes } from 'react-icons/fa';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // Stan do otwierania/zamykania menu
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className={styles.navbar}>
             <Link to="/" className={styles.title}>Te Podróże</Link>
-            <ul className={styles.navLinks}>
-                <li><Link to="/"><FaHome className={styles.icon} /> Home</Link></li>
-                <li><Link to="/about"><FaUser className={styles.icon} /> O mnie</Link></li>
-                <li><Link to="/contact"><FaEnvelope className={styles.icon} /> Kontakt</Link></li>
+
+            {/* Przycisk hamburgera */}
+            <button className={styles.hamburger} onClick={toggleMenu}>
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+
+            {/* Menu */}
+            <ul className={`${styles.navLinks} ${isMenuOpen ? styles.open : ''}`}>
+                <li><Link to="/" onClick={toggleMenu}><FaHome className={styles.icon} /> Home</Link></li>
+                <li><Link to="/about" onClick={toggleMenu}><FaUser className={styles.icon} /> O mnie</Link></li>
+                <li><Link to="/contact" onClick={toggleMenu}><FaEnvelope className={styles.icon} /> Kontakt</Link></li>
             </ul>
         </nav>
     );
